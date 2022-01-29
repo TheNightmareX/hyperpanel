11e2165386d0589790af4858786279a999ca226a
Char2s <Char2s@outlook.com>
2022-01-29 21:36:27 +0800

    feat(backend/app): init GraphQL

--- projects/hyperpanel-backend/src/app.module.ts
+++ projects/hyperpanel-backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module, NotFoundException } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { DB_PATH, DEBUG } from './env.constants';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      debug: DEBUG,
+      playground: DEBUG,
+    }),
     MikroOrmModule.forRoot({
       type: 'sqlite',
       dbName: DB_PATH,
