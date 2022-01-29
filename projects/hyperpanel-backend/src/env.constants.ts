d227775ff45e3482caad5786e9ca56a63aa1cacc
Char2s <Char2s@outlook.com>
2022-01-29 21:30:48 +0800

    feat(backend/env): implement parsers

--- projects/hyperpanel-backend/src/env.constants.ts
+++ projects/hyperpanel-backend/src/env.constants.ts
@@ -1,6 +1,24 @@
 import { config } from 'dotenv';
+import { env } from 'process';
 
 config();
 
-export const PORT = Number(process.env.PORT);
-export const DB_PATH = process.env.DB_PATH;
+export const PORT = num('PORT');
+export const DB_PATH = str('DB_PATH');
+
+function num(name: string) {
+  const value = Number(env[name]);
+  if (isNaN(value)) throw `Environment variable "${name}" must be a number`;
+  return value;
+}
+
+function str(name: string) {
+  return env[name];
+}
+
+function bool(name: string) {
+  const raw = env[name];
+  const value = raw == 'true' ? true : raw == 'false' ? false : null;
+  if (value == null) throw `Environment variable "${name}" must be a boolean`;
+  return value;
+}
