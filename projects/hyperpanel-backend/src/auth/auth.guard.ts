30f5b342b91929fc6e2d89f5ef08a1423645f1e4
Char2s <Char2s@outlook.com>
2022-02-03 01:06:17 +0800

    feat(backend/auth): implement guard decorator `skip`

--- projects/hyperpanel-backend/src/auth/auth.guard.ts
+++ projects/hyperpanel-backend/src/auth/auth.guard.ts
@@ -1,20 +1,36 @@
 import {
   CanActivate,
+  CustomDecorator,
   ExecutionContext,
   Injectable,
+  SetMetadata,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { JwtService } from '@nestjs/jwt';
 import { ExpressContext } from 'apollo-server-express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  static skip = (value = true): CustomDecorator<typeof AuthGuard['skip']> =>
+    SetMetadata(AuthGuard.skip, value);
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request =
-      GqlExecutionContext.create(context).getContext<ExpressContext>().req;
+  constructor(
+    private reflector: Reflector,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async canActivate(contextRaw: ExecutionContext): Promise<boolean> {
+    const context = GqlExecutionContext.create(contextRaw);
+
+    const shouldSkip = this.reflector.getAllAndOverride(AuthGuard.skip, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (shouldSkip) return true;
+
+    const request = context.getContext<ExpressContext>().req;
     const token = this.parseToken(request.headers.authorization ?? '');
     if (!token) throw new UnauthorizedException();
     await this.jwtService.verifyAsync(token).catch(() => {
