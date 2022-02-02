c95f5284270459dd1349d9cff815b171b4d3d739
Char2s <Char2s@outlook.com>
2022-02-03 01:06:43 +0800

    feat(backend/auth): skip auth guard for query `authorization`

--- projects/hyperpanel-backend/src/auth/auth.resolver.ts
+++ projects/hyperpanel-backend/src/auth/auth.resolver.ts
@@ -1,11 +1,13 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
 
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
+  @AuthGuard.skip(true)
   @Query(() => String)
   async authorization(
     @Args('username') username: string,
