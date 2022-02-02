a6ad915625cb3ffa12f6989ba5701aacf5b13ed4
Char2s <Char2s@outlook.com>
2022-02-03 00:03:30 +0800

    feat(backend/auth): scaffold

--- projects/hyperpanel-backend/src/app.module.ts
+++ projects/hyperpanel-backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module, NotFoundException } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
+import { AuthModule } from './auth/auth.module';
 import { DB_PATH, DEBUG } from './env.constants';
 import { FilesModule } from './files/files.module';
 
@@ -24,6 +25,7 @@ import { FilesModule } from './files/files.module';
       debug: DEBUG,
     }),
     FilesModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
