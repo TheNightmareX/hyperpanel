63b40c1f20a5839546386bd546b3fe99b979e69f
Char2s <Char2s@outlook.com>
2022-02-04 00:58:24 +0800

    refactor(frontend/app): remove redundant module imports

--- projects/hyperpanel-frontend/src/app/app.module.ts
+++ projects/hyperpanel-frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
@@ -20,7 +19,6 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     HttpClientModule,
     NzIconModule.forRoot([]),
     GraphQLModule,
