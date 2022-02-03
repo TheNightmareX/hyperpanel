bf8f56e79d050d44381a326d2857ce3ba8b56c41
Char2s <Char2s@outlook.com>
2022-02-03 18:02:33 +0800

    feat(frontend/app): register root icons

--- projects/hyperpanel-frontend/src/app/app.module.ts
+++ projects/hyperpanel-frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,10 +18,11 @@ registerLocaleData(en);
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
+    NzIconModule.forRoot([]),
+    AppRoutingModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
