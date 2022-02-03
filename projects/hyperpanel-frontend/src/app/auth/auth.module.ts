291b140c1100e889d940daddf0eb5c964ac14af0
Char2s <Char2s@outlook.com>
2022-02-03 12:34:52 +0800

    style(frontend): run ESLint

--- projects/hyperpanel-frontend/src/app/auth/auth.module.ts
+++ projects/hyperpanel-frontend/src/app/auth/auth.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [AuthComponent],
