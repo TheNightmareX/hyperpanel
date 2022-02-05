8fcdb8aa24d38c4d23783c3a78977ae0104f94c2
Char2s <Char2s@outlook.com>
2022-02-05 18:25:01 +0800

    feat(frontend/auth): navigate after successfully authed

--- projects/hyperpanel-frontend/src/app/auth/auth.component.ts
+++ projects/hyperpanel-frontend/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 
 import { AuthService } from './auth.service';
@@ -15,6 +16,7 @@ export class AuthComponent implements OnInit {
   };
 
   constructor(
+    private router: Router,
     private messageService: NzMessageService,
     private authService: AuthService,
   ) {}
@@ -23,8 +25,13 @@ export class AuthComponent implements OnInit {
 
   async login(username: string, password: string): Promise<void> {
     this.authService.login(username, password).subscribe({
-      next: () => this.messageService.success('Login succeeded'),
-      error: () => this.messageService.error('Login failed'),
+      next: () => {
+        this.messageService.success('Login succeeded');
+        this.router.navigate(['/panel']);
+      },
+      error: () => {
+        this.messageService.error('Login failed');
+      },
     });
   }
 }
