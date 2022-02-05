b33051d722addd6c3fdf8b144176b973a34ea3b3
Char2s <Char2s@outlook.com>
2022-02-06 01:23:51 +0800

    feat(frontend/panel): update sidebar links

--- projects/hyperpanel-frontend/src/app/panel/panel.module.ts
+++ projects/hyperpanel-frontend/src/app/panel/panel.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconDefinition } from '@ant-design/icons-angular';
+import { FileOutline } from '@ant-design/icons-angular/icons';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
@@ -9,7 +10,7 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { PanelComponent } from './panel.component';
 import { PanelRoutingModule } from './panel-routing.module';
 
-const icons: IconDefinition[] = [];
+const icons: IconDefinition[] = [FileOutline];
 
 @NgModule({
   declarations: [PanelComponent],
