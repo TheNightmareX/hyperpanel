33cfda391f84d81aecf0c0cc03092d518ae94d4a
Char2s <Char2s@outlook.com>
2022-02-05 12:40:34 +0800

    feat(frontend/auth): store token to local storage

--- projects/hyperpanel-frontend/src/app/auth/auth.service.ts
+++ projects/hyperpanel-frontend/src/app/auth/auth.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
-import { filter, map, Observable } from 'rxjs';
+import { filter, map, Observable, tap } from 'rxjs';
 
+import { LocalStorageItem } from '../common/local-storage-item.class';
 import { AuthorizeGQL } from '../graphql';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  token = new LocalStorageItem<string | null>(
+    'token',
+    (v) => typeof v == 'string',
+    null,
+  );
+
   constructor(private authorizeGql: AuthorizeGQL) {}
 
   login(username: string, password: string): Observable<string> {
     return this.authorizeGql.mutate({ username, password }).pipe(
       map((result) => result.data?.authorize),
       filter((token): token is string => !!token),
+      tap((token) => this.token.next(token).save()),
     );
   }
 }
