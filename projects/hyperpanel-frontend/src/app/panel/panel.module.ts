ffdac5444627b75c32a2e022ce0764ab960e6a96
Char2s <Char2s@outlook.com>
2022-02-06 01:03:12 +0800

    feat(frontend/panel): implement layout UI

--- projects/hyperpanel-frontend/src/app/panel/panel.module.ts
+++ projects/hyperpanel-frontend/src/app/panel/panel.module.ts
@@ -1,11 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 import { PanelComponent } from './panel.component';
 import { PanelRoutingModule } from './panel-routing.module';
 
+const icons: IconDefinition[] = [];
+
 @NgModule({
   declarations: [PanelComponent],
-  imports: [CommonModule, PanelRoutingModule],
+  imports: [
+    CommonModule,
+    PanelRoutingModule,
+    NzIconModule.forChild(icons),
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+  ],
 })
 export class PanelModule {}
