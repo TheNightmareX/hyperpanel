e5f1239de2b5487786999491ae79580a6581470a
Char2s <Char2s@outlook.com>
2022-02-05 12:09:49 +0800

    refactor(frontend/common): adjust implementation of class `LocalStorageItem`

--- projects/hyperpanel-frontend/src/app/common/local-storage-item.class.ts
+++ projects/hyperpanel-frontend/src/app/common/local-storage-item.class.ts
@@ -1,25 +1,14 @@
-import { BehaviorSubject, Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
-
-const INITIAL = Symbol();
+import { BehaviorSubject } from 'rxjs';
 
 export class LocalStorageItem<Value> {
-  get value(): Value {
-    return this._value;
-  }
-  private _value!: Value;
-
-  value$: Observable<Value>;
-  private _value$ = new BehaviorSubject<Value | typeof INITIAL>(INITIAL);
+  value!: Value;
+  value$!: BehaviorSubject<Value>;
 
   constructor(
     public key: string,
     private validator: (dirty: unknown) => boolean,
     private initial: Value | (() => Value),
   ) {
-    this.value$ = this._value$
-      .asObservable()
-      .pipe(filter((value): value is Value => value != INITIAL));
     this.load().save();
   }
 
@@ -29,8 +18,9 @@ export class LocalStorageItem<Value> {
    * @returns
    */
   next<Next extends Value>(value: Next): LocalStorageItem<Next> {
-    this._value = value;
-    this._value$.next(value);
+    this.value = value;
+    if (this.value$) this.value$.next(value);
+    else this.value$ = new BehaviorSubject<Value>(value);
     return this as unknown as LocalStorageItem<Next>;
   }
 
@@ -53,11 +43,11 @@ export class LocalStorageItem<Value> {
         error == VALUE_NOT_EXIST ||
         error == VALUE_NOT_VALID ||
         error instanceof SyntaxError
-      )
-        this.next(
-          this.initial instanceof Function ? this.initial() : this.initial,
-        );
-      else throw error;
+      ) {
+        const initial =
+          this.initial instanceof Function ? this.initial() : this.initial;
+        this.next(initial);
+      } else throw error;
     } finally {
       return this;
     }
