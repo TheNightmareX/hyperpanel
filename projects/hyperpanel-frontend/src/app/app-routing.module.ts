d2477635806e98a798743583ff0474e23f59f31e
Char2s <Char2s@outlook.com>
2022-02-05 18:26:36 +0800

    feat(frontend/app/routing): add root redirection

--- projects/hyperpanel-frontend/src/app/app-routing.module.ts
+++ projects/hyperpanel-frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'panel', pathMatch: 'full' },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
