01cbb6c4e02ed79c607457743906d5c7620cba5c
Char2s <Char2s@outlook.com>
2022-02-07 21:25:21 +0800

    feat(frontend/files): implement basic layout

--- projects/hyperpanel-frontend/src/app/files/files.module.ts
+++ projects/hyperpanel-frontend/src/app/files/files.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
+import { FileTableComponent } from './file-table/file-table.component';
 import { FilesComponent } from './files.component';
 import { FilesRoutingModule } from './files-routing.module';
 
 @NgModule({
-  declarations: [FilesComponent],
-  imports: [CommonModule, FilesRoutingModule],
+  declarations: [FilesComponent, FileTableComponent],
+  imports: [CommonModule, FilesRoutingModule, NzCardModule],
 })
 export class FilesModule {}
