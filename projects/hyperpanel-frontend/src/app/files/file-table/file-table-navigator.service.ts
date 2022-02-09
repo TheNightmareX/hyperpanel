d45aae2ba6bc89132c1267c5a469239a919f2422
Char2s <Char2s@outlook.com>
2022-02-09 12:56:16 +0800

    feat(frontend/files/table/navigator): record history

--- projects/hyperpanel-frontend/src/app/files/file-table/file-table-navigator.service.ts
+++ projects/hyperpanel-frontend/src/app/files/file-table/file-table-navigator.service.ts
@@ -5,12 +5,16 @@ import { map, Observable } from 'rxjs';
 @Injectable()
 export class FileTableNavigator {
   path$: Observable<string>;
+  histories: string[] = [];
+  position = -1;
 
   constructor(private router: Router, private route: ActivatedRoute) {
     this.path$ = this.route.params.pipe(map((params) => params['path'] ?? '/'));
   }
 
   navigate(path: string): void {
+    this.position++;
+    this.histories[this.position] = path;
     this.router.navigate([{ path }], { relativeTo: this.route });
   }
 }
