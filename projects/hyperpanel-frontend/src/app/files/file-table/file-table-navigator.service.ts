ac53bcbf3bf7a01c55eb56163f31c5ba697652eb
Char2s <Char2s@outlook.com>
2022-02-09 13:21:51 +0800

    feat(frontend/files/table/navigator): implement forwarding and backwarding

--- projects/hyperpanel-frontend/src/app/files/file-table/file-table-navigator.service.ts
+++ projects/hyperpanel-frontend/src/app/files/file-table/file-table-navigator.service.ts
@@ -5,16 +5,38 @@ import { map, Observable } from 'rxjs';
 @Injectable()
 export class FileTableNavigator {
   path$: Observable<string>;
-  histories: string[] = [];
-  position = -1;
+  histories: string[] = ['/'];
+  position = 0;
+
+  get canForward(): boolean {
+    return this.position < this.histories.length - 1;
+  }
+
+  get canBackward(): boolean {
+    return this.position > 0;
+  }
 
   constructor(private router: Router, private route: ActivatedRoute) {
     this.path$ = this.route.params.pipe(map((params) => params['path'] ?? '/'));
   }
 
   navigate(path: string): void {
-    this.position++;
+    this.histories.length = ++this.position;
     this.histories[this.position] = path;
-    this.router.navigate([{ path }], { relativeTo: this.route });
+    this.router.navigate([{ path }], {
+      relativeTo: this.route,
+    });
+  }
+
+  forward(): void {
+    if (!this.canForward) return;
+    this.position++;
+    history.forward();
+  }
+
+  backward(): void {
+    if (!this.canBackward) return;
+    this.position--;
+    history.back();
   }
 }
