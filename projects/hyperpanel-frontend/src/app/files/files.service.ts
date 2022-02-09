8be589c9a7b44611bb7c1b2f8e3ad8607d87f9fa
Char2s <Char2s@outlook.com>
2022-02-10 01:03:23 +0800

    feat(frontend/files/service): implement method `save`

--- projects/hyperpanel-frontend/src/app/files/files.service.ts
+++ projects/hyperpanel-frontend/src/app/files/files.service.ts
@@ -1,8 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FilesService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  // TODO: support large files. (Probably by forwarding a `ReadableStream`)
+  save(path: string, name: string): void {
+    const url = `${environment.serverUrl}/files${path}`;
+    this.httpClient.get(url, { responseType: 'blob' }).subscribe((blob) => {
+      const blobUrl = URL.createObjectURL(blob);
+      const $a = document.createElement('a');
+      $a.href = blobUrl;
+      $a.download = name;
+      $a.click();
+      URL.revokeObjectURL(blobUrl);
+    });
+  }
 }
