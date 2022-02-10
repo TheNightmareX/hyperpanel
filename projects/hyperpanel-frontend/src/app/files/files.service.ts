57deb061ed0a0e35f734c80cb305b570eea84acd
Char2s <Char2s@outlook.com>
2022-02-10 13:23:03 +0800

    feat(frontend/files): support large file downloading by directly forwarding the body stream to the file system

--- projects/hyperpanel-frontend/src/app/files/files.service.ts
+++ projects/hyperpanel-frontend/src/app/files/files.service.ts
@@ -1,23 +1,22 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import streamSaver from 'streamsaver';
+
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FilesService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
-  // TODO: support large files. (Probably by forwarding a `ReadableStream`)
-  save(path: string, name: string): void {
+  // TODO: cancel the streams when user canceled the download.
+  async save(path: string, name: string): Promise<void> {
     const url = `${environment.serverUrl}/files${path}`;
-    this.httpClient.get(url, { responseType: 'blob' }).subscribe((blob) => {
-      const blobUrl = URL.createObjectURL(blob);
-      const $a = document.createElement('a');
-      $a.href = blobUrl;
-      $a.download = name;
-      $a.click();
-      URL.revokeObjectURL(blobUrl);
+    const response = await fetch(url, {
+      headers: { ['Authorization']: `Bearer ${this.authService.token.value}` },
     });
+    const writableStream = streamSaver.createWriteStream(name);
+    return response.body?.pipeTo(writableStream);
   }
 }
