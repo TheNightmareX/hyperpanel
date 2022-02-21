8eb52b24702382d81e3d6a618898a58034152534
Char2s <Char2s@outlook.com>
2022-02-21 19:54:34 +0800

    refactor(frontend): correct file names

--- projects/hyperpanel-frontend/src/app/graphql/graphql-deferrer.interceptor.ts
+++ projects/hyperpanel-frontend/src/app/graphql/graphql-deferrer.interceptor.ts
@@ -7,7 +7,7 @@ import {
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { deferTime } from '../common/deferTime.operator';
+import { deferTime } from '../common/defer-time.operator';
 
 /**
  * @deprecated Temporary deprecated.
